68a1c152e2aed1e38d3d247a4de0e58ef449796b
1manfactory <1manfactory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-16 16:38:50 +0000

    more features
    git-svn-id: https://plugins.svn.wordpress.org/geotagmapper/trunk@253149 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- geotagmapper.php
+++ geotagmapper.php
@@ -1,12 +1,31 @@
 <?php
 /*
 Plugin Name: Geotagmapper
-Plugin URI: http://wordpress.org/extend/plugins/geotagmapper/
+Plugin URI: http://1manfactory.com/wordpress-plugin-geotagmapper-customize-geo-information-inside-header-automatically/
 Description: Geotagmapper adds geographical identification metadata (latitude and longitude) to the HTML header. You only have to specify an address.
-Version: 1.1
+Version: 1.2
 Author: J&uuml;rgen Schulze
 Author URI: http://1manfactory.com
+License: GNU GPL
 */
+
+/*  Copyright 2010 Juergen Schulze, 1manfactory.com (email : 1manfactory@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 
 DEFINE("DEBUG", "false");	# set "true" or "false", quotation marks needed
 
@@ -44,7 +63,7 @@ function my_plugin_options(){
 	<form method="post" action="options.php" id="my_plugin_form">';
 	wp_nonce_field('update-options');
 	print '<input type="hidden" name="action" value="update" />
-	<input type="hidden" name="page_options" value="geotagmapper_street,geotagmapper_lat,geotagmapper_lng,geotagmapper_zip,geotagmapper_country,geotagmapper_state,geotagmapper_city,geotagmapper_country_code,geotagmapper_subcountry_code,geotagmapper_track" />
+	<input type="hidden" name="page_options" value="geotagmapper_place_on_search,geotagmapper_place_on_tag,geotagmapper_place_on_category,geotagmapper_place_on_single_page,geotagmapper_place_on_single_post,geotagmapper_place_on_front,geotagmapper_street,geotagmapper_lat,geotagmapper_lng,geotagmapper_zip,geotagmapper_country,geotagmapper_state,geotagmapper_city,geotagmapper_country_code,geotagmapper_subcountry_code,geotagmapper_track" />
 	<table class="form-table">
 	<tr>
 		<th scope="row" valign="top">Fill in as many information as possible.</th>
@@ -75,6 +94,25 @@ function my_plugin_options(){
 			<label for="geotagmapper_country_code">ISO 3166-1/2 Codes for the representation of names of countries and their subdivisions</label><br />
 			<input size="2" maxlength="2" type="text" id="geotagmapper_country_code" name="geotagmapper_country_code" value="'.get_option('geotagmapper_country_code').'" />-<input size="2" maxlength="2" type="text" id="geotagmapper_subcountry_code" name="geotagmapper_subcountry_code" value="'.get_option('geotagmapper_subcountry_code').'" /><a href="http://www.unece.org/cefact/locode/service/sublocat.htm" target="_blank">Pick up</a> the value for the second field.<br />
 
+			<br />	
+			<label for="geotagmapper_zip">Insert Geodata into header of</label><br />
+			<input type="checkbox" id="geotagmapper_place_on_front" name="geotagmapper_place_on_front" '.my_plugin_optionselected(get_option('geotagmapper_place_on_front')).' />&nbsp;'.__('Front page').'&nbsp;&nbsp;
+			<input type="checkbox" id="geotagmapper_place_on_single_post" name="geotagmapper_place_on_single_post" '.my_plugin_optionselected(get_option('geotagmapper_place_on_single_post')).' />&nbsp;'.__('Single post').'&nbsp;&nbsp;
+			<input type="checkbox" id="geotagmapper_place_on_single_page" name="geotagmapper_place_on_single_page" '.my_plugin_optionselected(get_option('geotagmapper_place_on_single_page')).' />&nbsp;'.__('Single page').'
+			
+			<input type="checkbox" id="geotagmapper_place_on_category" name="geotagmapper_place_on_category" '.my_plugin_optionselected(get_option('geotagmapper_place_on_category')).' />&nbsp;'.__('Category archive').'
+			<input type="checkbox" id="geotagmapper_place_on_tag" name="geotagmapper_place_on_tag" '.my_plugin_optionselected(get_option('geotagmapper_place_on_tag')).' />&nbsp;'.__('Tag Archive').'
+			<input type="checkbox" id="geotagmapper_place_on_search" name="geotagmapper_place_on_search" '.my_plugin_optionselected(get_option('geotagmapper_place_on_search')).' />&nbsp;'.__('Search results').'
+
+			<br /><br />
+			Or use the code:
+			<pre>
+			&lt;?php
+			if (function_exists(\'geotagmapper_meta_print\')) geotagmapper_meta_print();
+			?&gt;
+			</pre>
+			to place the Geodata anywhere in the header.
+			
 		</td>
 	</tr>
 	</table>
@@ -87,8 +125,18 @@ function my_plugin_options(){
 }
 
 function geotagmapper_meta(){
+	if (get_option('geotagmapper_place_on_front') && is_front_page()) $printmeta=true;
+	if (get_option('geotagmapper_place_on_single_post') && is_single()) $printmeta=true;
+	if (get_option('geotagmapper_place_on_single_page') && is_page()) $printmeta=true;
+	if (get_option('geotagmapper_place_on_category') && is_category()) $printmeta=true;
+	if (get_option('geotagmapper_place_on_tag') && is_tag()) $printmeta=true;
+	if (get_option('geotagmapper_place_on_search') && is_search()) $printmeta=true;
+	if ($printmeta) geotagmapper_meta_print();
+}
+
+function geotagmapper_meta_print() {
 	print '
-	<!-- inserted by geotagmapper start -->
+	<!-- inserted by geotagmapper (Juergen Schulze, 1manfactory.com) start -->
 	<meta name="city" content="'.get_option('geotagmapper_city').'" />
 	<meta name="country" content="'.get_option('geotagmapper_country').'" />
 	<meta name="state" content="'.get_option('geotagmapper_state').'" />
@@ -105,9 +153,28 @@ function my_plugin_admin_init() {
 	wp_register_script('myPluginScript', WP_PLUGIN_URL . '/geotagmapper/geotagmapper.js');
 }
 
+function my_plugin_optionselected($checkValue) {
+	if($checkValue) {
+		return 'checked';
+	}
+	return '';
+}
+
+function geotagmapper_uninstall() {
+	# delete all data stored
+	delete_option('geotagmapper_place_on_front');
+	delete_option('geotagmapper_place_on_single_post');
+	delete_option('geotagmapper_place_on_single_page');
+	delete_option('geotagmapper_place_on_category');
+	delete_option('geotagmapper_place_on_tag');
+	delete_option('geotagmapper_place_on_search');
+}
+
 add_action('admin_init', 'my_plugin_admin_init');
 add_action('admin_menu', 'my_plugin_admin_menu');
 add_action('publish_post', 'geotagmapper_ping');
 add_action('admin_menu', 'my_plugin_admin_menu');
 add_action('wp_head', 'geotagmapper_meta');
+register_uninstall_hook(__FILE__, 'geotagmapper_uninstall');
+
 ?>
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 === Geotagmapper ===
-Contributors: Juergen Schulze
-Donate link: http://juergen-schulze.de
+Contributors: Juergen Schulze  (1manfactory.com)
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ESLEBR77J67A2
 Tags: geo, map, karte, blogmap, geotagmapper, blogkarte, google maps, maps, coordinates, google map, google, mashup, mapping, geocms, geographic, georss, location, geocoding
 Requires at least: 2.5
 Tested up to: 2.9.2
@@ -12,6 +12,8 @@ Description: Geotagmapper adds geographical identification metadata (latitude an
 
 Geotagmapper adds geographical identification metadata (latitude and longitude) to the HTML header.You only have to specify your address.
 
+Check out my other [Wordpress Plugins](http://wordpress.org/extend/plugins/profile/1manfactory)
+
 == Installation ==
 
 1. Copy complete folder to '/wp-content/plugins/' or upload from wordpress.org
@@ -21,9 +23,41 @@ Geotagmapper adds geographical identification metadata (latitude and longitude) 
 5. Save changes
 6. Check inside <HTML><HEAD></HEAD></HTML> of your webpage
 
+== Remove plugin ==
+
+1. Deactivate plugin through the 'Plugins' menu in WordPress
+2. Delete plugin through the 'Plugins' menu in WordPress
+
+It's best to use the build in delete function of wordpress. That way all the stored data will be removed and no orphaned data will stay.
+
+== Changelog ==
+
+= 1.2 (16.06.2010) =
+* ability to insert geodata in frontpage, pages or posts only
+* creating function to place data manually into templates
+
 == Frequently Asked Questions ==
 
-* none
+= How can I manually place the Geodata in headers? =
+
+Use the function "geotagmapper_meta_print". E.g. place this code inside your template file(s)
+`<?php if (function_exists('geotagmapper_meta_print')) geotagmapper_meta_print(); ?>`
+
+= What is the plugin page?  =
+
+[Geotagmapper](http://1manfactory.com/wordpress-plugin-geotagmapper-customize-geo-information-inside-header-automatically/ "Geotagmapper")
+
+= Do you have other plugins?  =
+
+Check out my other [Wordpress Plugins](http://wordpress.org/extend/plugins/profile/1manfactory)
+
+= Where do I post my feedback? =
+
+Post it at the plugin page: [Geotagmapper](http://1manfactory.com/wordpress-plugin-geotagmapper-customize-geo-information-inside-header-automatically/ "Geotagmapper")
+
+= How can I support you? =
+
+That is nice of you. Donation link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ESLEBR77J67A2
 
 == Screenshots ==
 
@@ -31,3 +65,11 @@ Geotagmapper adds geographical identification metadata (latitude and longitude) 
 2. Step 2
 3. Step 3
 4. Step 4
+
+== Upgrade Notice ==
+
+Just do a normal upgrade.
+
+== To do ==
+
+More translations. Does someone wants to help?
